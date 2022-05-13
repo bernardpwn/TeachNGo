45f8cb13132cebea8494b7f80c0622df08c6c81e
Bernard Pratama Wongso <56827357+bernardpwn@users.noreply.github.com>
2022-05-13 23:17:22 +0800

    adding main page for admin website
    index.php file for landing page

---
+++ index.php
@@ -0,0 +1,92 @@
+<header>
+    <!--Sweet Alert Codes-->
+    <!--JQuery CDN-->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <!--Sweet Alert CDN-->
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+</header>
+<?php 
+ 
+include 'config.php';
+ 
+error_reporting(0);
+ 
+session_start();
+ 
+if (isset($_SESSION['username'])) {
+    header("Location: main.php");
+}
+ 
+if (isset($_POST['submit'])) {
+    $username = $_POST['username'];
+    $password = md5($_POST['password']);
+    $sql = "SELECT * FROM techngo.Admin WHERE Admin_Username='$username' AND Admin_Pass='$password'";
+    $result = mysqli_query($conn, $sql);
+    if ($result->num_rows > 0) {
+        $row = mysqli_fetch_assoc($result);
+        $_SESSION['username'] = $row['username'];
+        header("Location: main.php");
+    } else {
+        echo '<style type="text/css">
+        #failed {
+            display: block;
+        }
+        </style>';
+    }
+    
+}
+ 
+?>
+ 
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ 
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+ 
+    <link rel="stylesheet" type="text/css" href="style.css">
+    
+    <title>TeachNGo Admin Website</title>
+    <style>
+        p.hidden {
+        display: none;
+    }
+</style>
+</head>
+<body>
+    <div class="alert alert-warning" role="alert">
+        <?php echo $_SESSION['error']?>
+    </div>
+ 
+    <div class="container">
+        <form action="" method="POST" class="login-admin">
+            <p class="login-text" style="font-size: 2rem; font-weight: 800;">Login</p>
+            <div class="input-group">
+                <input type="text" placeholder="Username" name="username" value="<?php echo $username; ?>" required>
+            </div>
+            <div class="input-group" >
+                <input id="passvisibility" type="password" placeholder="Password" name="password" value="<?php echo $_POST['password']; ?>" required>
+            </div>
+            <div class="input-group">
+                <button name="submit" class="btn">Login</button>
+            </div>
+            <input type="checkbox"  onclick="FunctionVisibility()">Show Password
+            <p class="hidden" id="failed" style="font-size: 1rem; font-weight: 500; text-align: center; color: red;">Username or Password Invalid !</p>
+        </form>
+    </div>
+    <script>
+        function FunctionVisibility() {
+            
+            var x = document.getElementById("passvisibility");
+            if (x.type === "password") {
+                x.type = "text";
+            } else {
+                x.type = "password";
+            }
+        }
+
+    </script>
+</body>
+</html>
