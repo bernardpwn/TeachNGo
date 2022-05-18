dde2480b33a076fb1eede28596a66e72c3e7fa73
hilmanfadly <93122393+hilmanfadly@users.noreply.github.com>
2022-05-18 17:52:24 +0700

    Update Index and Style

--- index.php
+++ index.php
@@ -24,7 +24,7 @@ if (isset($_POST['submit'])) {
     $result = mysqli_query($conn, $sql);
     if ($result->num_rows > 0) {
         $row = mysqli_fetch_assoc($result);
-        $_SESSION['username'] = $row['username'];
+        $_SESSION['username'] = $row['Admin_Id'];
         header("Location: main.php");
     } else {
         echo '<style type="text/css">
