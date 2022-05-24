132c32eafa81820bf377d649ca0c988945ff4eef
Bernard Pratama Wongso <56827357+bernardpwn@users.noreply.github.com>
2022-05-24 15:07:24 +0800

    Update classs and search PHP for Mobile

--- search.php
+++ search.php
@@ -13,12 +13,12 @@
 	  die("Connection failed: " . mysqli_connect_error());
 	}
     $response = array();
-    if(isset($_POST['subject_name'])){
-		$subject_name=$_POST['subject_name'];
-		$q=mysqli_query($conn,"SELECT * FROM Class JOIN Subject ON Class.Subject_Id = Subject.Subject_Id JOIN User ON User.User_Id= Class.User_Id WHERE Subject_Name='$subject_name' AND Class_Status = 'Active'");
+    if(isset($_POST['search'])){
+		$search=$_POST['search'];
+		$q=mysqli_query($conn,"SELECT * FROM Class JOIN Subject ON Class.Subject_Id = Subject.Subject_Id JOIN User ON User.User_Id= Class.User_Id WHERE User_Name LIKE '%$search%' AND Class_Status = 'Active'");
 		
         $response["data"] = array();
-  
+
         while($r = $q -> fetch_assoc()){
             $mhs = array();
             $mhs["Class_Id"] = $r["Class_Id"];
@@ -28,13 +28,20 @@
 			$mhs["User_Photo"] = $r["User_Photo"];
             array_push($response["data"], $mhs);
         }
-        $response["status"] = 1;
-        $response["message"] = "Data class berhasil dibaca";
-        echo json_encode($response);
+        if($mhs != null){
+            $response["status"] = 1;
+            $response["message"] = "Data search berhasil dibaca";
+            echo json_encode($response);
+        } else {
+            $response["status"] = 0;
+            $response["message"] = "Data class tidak ditemukan";
+            echo json_encode($response);
+        }
+        
     }
     else{
-        $response["status"] = 0;
-        $response["message"] = "Tidak ada data";
+        $response["status"] = -1;
+        $response["message"] = "Keyword search tidak ada";
         echo json_encode($response);
     }
 ?>
